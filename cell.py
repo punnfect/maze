86ebb096156dc8fb00fb16b100f5e2ea8a2a1ac1
punnfect <sp.kool@yahoo.com>
2024-06-20 23:22:25 -0500

    point, line, and cell class added

---
+++ cell.py
@@ -0,0 +1,30 @@
+from graphics import Line, Point
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            l = Line(Point(self._x1, self._y1), Point(self._x1, self._y2))
+            self._win.draw_line(l, "black")
+        if self.has_top_wall:
+            l = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
+            self._win.draw_line(l, "black")
+        if self.has_right_wall:
+            l = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
+            self._win.draw_line(l, "black")
+        if self.has_bottom_wall:
+            l = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
+            self._win.draw_line(l, "black")
--- maze.py
+++ maze.py
@@ -2,10 +2,21 @@
 # has to be run ./file.py will only run sudo python3 file.py
 # tkinter is only attached to sudo for some reason
 # will figure out why later
-from graphics import Window
+from graphics import Window, Line, Point
+from cell import Cell
 
 def main():
     win = Window(800, 600)
+    
+    c = Cell(win)
+    c.draw(1, 1, 100, 100)
+    c = Cell(win)
+    c.draw(100, 1, 200, 100)
+    c = Cell(win)
+    c.draw(200, 1, 300, 100)
+    c = Cell(win)
+    c.draw(300, 1, 400, 100)
+
     win.wait_for_close()
 
 main()
