8b76567043df007a80ac23a0a43824a339e0d9d4
punnfect <sp.kool@yahoo.com>
2024-06-24 17:16:22 -0500

    visual changes unimportant

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from graphics import Window
 from maze import Maze
 
 def main():
-    num_rows = 5
-    num_cols = 5
+    num_rows = 25
+    num_cols = 25
     margin = 50
-    screen_x = 800
-    screen_y = 600
+    screen_x = 1000
+    screen_y = 1000
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
     win = Window(screen_x, screen_y)
 
-    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     maze.solve()
     win.wait_for_close()
 
